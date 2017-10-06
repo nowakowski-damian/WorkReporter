f2f94bd0d4f0e4f39e5a5a4a33edd8eace562256
damian <thirteendollars.com@gmail.com>
2017-10-07 00:44:20 +0200

    Added database sql dump file

---
+++ db_dump/work_reporter_empty.sql
@@ -0,0 +1,103 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+        SET time_zone = "+00:00";
+
+
+
+        /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+
+        /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+
+        /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+
+        /*!40101 SET NAMES utf8 */;
+
+        --
+-- Database: `work_reporter`
+--
+
+
+
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `blocked` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+
+
+
+
+
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) DEFAULT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text,
+  `blocked` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `customer_id` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+
+
+
+
+
+CREATE TABLE `report` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time_hours` decimal(10,2) NOT NULL,
+  `description` text,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`,`project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+
+
+
+
+
+CREATE TABLE `report_affirmation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `vacation` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `account_type` varchar(10) NOT NULL DEFAULT 'user',
+  `blocked` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `id_2` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+
+INSERT INTO user VALUES
+("1","root","$2y$10$HfsIQLrn1/KrFYVXfksUseXVY1Si2F8Sz7TYFYwxOQcM.GtV9fBUm","-","-","root","0");
+
+
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+        /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+
+        /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
